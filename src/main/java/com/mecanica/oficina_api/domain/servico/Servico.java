92046c7fb11fafbf5611b99602b007e5af858f52
gabrielppm <gabrielppm@protonmail.com>
2026-05-01 13:09:35 -0300

    adicionando dominio servico.java e persistence ServicoJpaEntity.java

---
+++ src/main/java/com/mecanica/oficina_api/domain/servico/Servico.java
@@ -0,0 +1,57 @@
+package com.mecanica.oficina_api.domain.servico;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Objects;
+
+import lombok.Getter;
+
+@Getter
+public class Servico {
+    private String id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private Duration tempoEstimadoHoras;
+    private boolean ativo;
+
+    protected Servico() {}
+
+    public static Servico criar(String nome, String descricao, BigDecimal preco, Duration tempoEstimadoHoras) {
+        
+        Servico servico = new Servico();
+        validarParametros(nome, descricao, preco, tempoEstimadoHoras);
+
+        servico.id = java.util.UUID.randomUUID().toString();
+        servico.nome = nome;
+        servico.descricao = descricao;
+        servico.preco = preco;
+        servico.tempoEstimadoHoras = tempoEstimadoHoras;
+        servico.ativo = true;
+
+        return servico;
+    }
+
+    public static void validarParametros(String nome, String descricao, BigDecimal preco, Duration tempoEstimadoHoras) {
+        if(Objects.isNull(nome) || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome é obrigatório");
+        }
+        if(Objects.isNull(descricao) || descricao.isBlank()) {
+            throw new IllegalArgumentException("Descrição é obrigatória");
+        }
+        if(Objects.isNull(preco) || preco.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Preço deve ser um número positivo");
+        }
+        if(Objects.isNull(tempoEstimadoHoras) || tempoEstimadoHoras.isNegative()) {
+            throw new IllegalArgumentException("Tempo Estimado deve ser um valor positivo");
+        }
+    }
+
+    public void ativar() {
+        this.ativo = true;
+    }
+    public void inativar() {
+        this.ativo = false;
+    }
+
+}
---
+++ src/main/java/com/mecanica/oficina_api/infrastructure/persistence/ServicoJpaEntity.java
@@ -0,0 +1,39 @@
+package com.mecanica.oficina_api.infrastructure.persistence;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "servicos")
+@Getter
+@Setter
+public class ServicoJpaEntity {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal preco;
+
+    @Column(nullable = false, name = "tempo_estimado_horas")
+    private Duration tempoEstimadoHoras;
+
+    @Column(nullable = false)
+    private boolean ativo;
+}
