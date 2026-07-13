4960a77eb13e0d84a8ee3cfc84a2c7203ea5f778
GabrielPPM <gabrielppm@protonmail.com>
2026-07-12 22:25:16 -0300

    feature de notificacao do status da OS via e-mail. Ainda sem opcao de aprocar ou rejeitar. Necessario adicionar cobertura de testes unitarios.

--- oficina-application/src/main/java/com/mecanica/oficina_api/application/ordemservico/usecase/IniciarDiagnosticoOrdemServicoUseCase.java
+++ oficina-application/src/main/java/com/mecanica/oficina_api/application/ordemservico/usecase/IniciarDiagnosticoOrdemServicoUseCase.java
@@ -1,18 +1,24 @@
 package com.mecanica.oficina_api.application.ordemservico.usecase;
 
+import com.mecanica.oficina_api.application.ordemservico.gateway.NotificadorClienteGateway;
 import com.mecanica.oficina_api.application.ordemservico.gateway.OrdemServicoGateway;
+import com.mecanica.oficina_api.application.ordemservico.output.NotificacaoCliente;
 import com.mecanica.oficina_api.domain.ordemservico.OrdemServico;
 
-public class IniciarDiagnosticoOrdemServicoUseCase {
-    private final OrdemServicoGateway ordemServicoGateway;
+    public class IniciarDiagnosticoOrdemServicoUseCase {
+        private final OrdemServicoGateway ordemServicoGateway;
+        private final NotificadorClienteGateway notificadorClienteGateway;
 
-    public IniciarDiagnosticoOrdemServicoUseCase(OrdemServicoGateway ordemServicoGateway) {
-        this.ordemServicoGateway = ordemServicoGateway;
-    }
+        public IniciarDiagnosticoOrdemServicoUseCase(OrdemServicoGateway ordemServicoGateway, NotificadorClienteGateway notificadorClienteGateway) {
+            this.ordemServicoGateway = ordemServicoGateway;
+            this.notificadorClienteGateway = notificadorClienteGateway;
+        }
 
-    public OrdemServico executar(String id) {
-        OrdemServico os = ordemServicoGateway.encontrarOuLancar(id);
-        os.iniciarDiagnostico();
-        return ordemServicoGateway.atualizar(os);
+        public OrdemServico executar(String id) {
+            OrdemServico os = ordemServicoGateway.encontrarOuLancar(id);
+            os.iniciarDiagnostico();
+            OrdemServico salva = ordemServicoGateway.atualizar(os);
+            notificadorClienteGateway.notificar(NotificacaoCliente.emDiagnostico(salva));
+            return salva;
+        }
     }
-}
